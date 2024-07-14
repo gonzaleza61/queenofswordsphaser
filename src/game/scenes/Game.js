28896a63439067ec00d7f2e5d69dbf6d78619453
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2024-07-14 00:07:20 -0500

    ground img added

--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -11,11 +11,21 @@ export class Game extends Scene {
 
         this.load.image("qos", "queenofswords.png");
         this.load.image("background", "bg.png");
+        this.load.image("ground", "platform.png");
     }
 
     create() {
+        var platforms;
         this.add.image(512, 384, "background");
-        this.add.image(40, 712, "qos");
+        this.add.image(40, 550, "qos");
+
+        platforms = this.physics.add.staticGroup();
+
+        platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+        platforms.create(600, 400, "ground");
+        platforms.create(50, 250, "ground");
+        platforms.create(750, 220, "ground");
 
         EventBus.emit("current-scene-ready", this);
     }
