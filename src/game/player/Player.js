9ff67c42d6a81636f0fc21ef14e4ad55aafd2036
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2024-07-24 22:44:26 -0500

    test

--- src/game/player/Player.js
+++ src/game/player/Player.js
@@ -6,7 +6,7 @@ class Player extends GameObjects.Sprite {
         this.scene.add.existing(this);
         this.scene.physics.add.existing(this); // Ensure physics is added
         this.body.setCollideWorldBounds(true);
-        this.body.setBounce(0.2);
+        this.body.setBounce(0);
     }
 }
 
