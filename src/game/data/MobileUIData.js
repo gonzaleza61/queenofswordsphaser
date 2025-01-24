3f9900a468231b06ffe1d2a8fec0633e382be624
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2025-01-24 00:11:07 -0600

    mobile UI refactor

---
+++ src/game/data/MobileUIData.js
@@ -0,0 +1,12 @@
+export const MobileUIData = {
+    restartControl: { x: 920, y: 50, key: "restart", scale: 1, alpha: 0.8 },
+    leftControl: { x: 80, y: 460, key: "left", scale: 1.3, alpha: 0.8 },
+    rightControl: { x: 200, y: 460, key: "right", scale: 1.3, alpha: 0.8 },
+    jumpControl: { x: 800, y: 460, key: "jump", scale: 1.3, alpha: 0.8 },
+    attackControl: { x: 920, y: 460, key: "attack", scale: 1.3, alpha: 0.8 },
+    leftDash: { x: 80, y: 365, key: "leftDash", scale: 1, alpha: 0.8 },
+    rightDash: { x: 200, y: 365, key: "rightDash", scale: 1, alpha: 0.8 },
+    musicOn: { x: 860, y: 50, key: "musicOn", scale: 1, alpha: 0.8 },
+    musicOff: { x: 860, y: 50, key: "musicOff", scale: 1, alpha: 0 },
+};
+
--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -2,8 +2,8 @@ import { Scene } from "phaser";
 import Phaser from "phaser";
 import { EventBus } from "../EventBus";
 import Player from "../player/Player";
-import MobileUILayout from "../mobileUI/MobileUI";
 import MobileButton from "../mobileUI/MobileUIButton";
+import { MobileUIData } from "../data/MobileUIData";
 export class Game extends Scene {
     constructor() {
         super("Game");
@@ -223,29 +223,17 @@ export class Game extends Scene {
 
         this.player.play("idle", true);
 
-        this.restartControl = new MobileButton(
-            this,
-            920,
-            50,
-            "restart",
-            1,
-            0.8
-        );
-        this.leftControl = new MobileButton(this, 80, 460, "left", 1.3, 0.8);
-        this.rightControl = new MobileButton(this, 200, 460, "right", 1.3, 0.8);
-        this.jumpControl = new MobileButton(this, 800, 460, "jump", 1.3, 0.8);
-        this.attackControl = new MobileButton(
-            this,
-            920,
-            460,
-            "attack",
-            1.3,
-            0.8
-        );
-        this.leftDash = new MobileButton(this, 80, 365, "leftDash", 1, 0.8);
-        this.rightDash = new MobileButton(this, 200, 365, "rightDash", 1, 0.8);
-        this.musicOn = new MobileButton(this, 860, 50, "musicOn", 1, 0.8);
-        this.musicOff = new MobileButton(this, 860, 50, "musicOff", 1, 0);
+        for (const [controlName, config] of Object.entries(MobileUIData)) {
+            this[controlName] = new MobileButton(
+                this,
+                config.x,
+                config.y,
+                config.key,
+                config.scale,
+                config.alpha
+            );
+        }
+
         this.isMusicOff = false;
         this.isLeftPressed = false;
         this.isRightPressed = false;
