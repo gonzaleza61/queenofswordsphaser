56c009a83bb306d275efece5af7c1fcd596abe3b
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2025-03-30 13:38:29 -0500

    refactor

--- src/game/enemies/Scorpio.js
+++ src/game/enemies/Scorpio.js
@@ -11,6 +11,40 @@ class Scorpio extends GameObjects.Sprite {
         this.body.setSize(30, 25);
         this.body.setOffset(16, 24);
         this.scene.physics.add.collider(this, this.scene.platformBlocks);
+
+        this.anims.create({
+            key: "scorpioIdle",
+            frames: this.anims.generateFrameNumbers("ScorpioIdle", {
+                frames: [0, 1, 2, 3],
+            }),
+            frameRate: 4,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "scorpioWalk",
+            frames: this.anims.generateFrameNumbers("ScorpioWalk", {
+                frames: [0, 1, 2, 3],
+            }),
+            frameRate: 8,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "scorpioAttack",
+            frames: this.anims.generateFrameNumbers("ScorpioAttack", {
+                frames: [0, 1, 2, 3],
+            }),
+        });
+
+        this.anims.create({
+            key: "scorpioDeath",
+            frames: this.anims.generateFrameNumbers("ScorpioDeath", {
+                frames: [0, 1, 2, 3],
+            }),
+        });
+
+        this.play("scorpioIdle", true);
     }
 }
 
